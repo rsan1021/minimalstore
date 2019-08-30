33f57c26b3396f1b3736d3145d9ec20ae3e2f1c5
Richard Sanchez <rsanchez973@live.com>
2019-08-30 13:02:19 -0500

    fixed child-theme
    fixed child-theme

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,6 +20,8 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+define('WP_CACHE', true);
+define( 'WPCACHEHOME', 'C:\MAMP\htdocs\minimalstore\wordpress\wp-content\plugins\wp-super-cache/' );
 define( 'DB_NAME', 'minimalstore' );
 
 /** MySQL database username */
